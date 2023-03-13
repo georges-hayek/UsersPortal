dd814de92e9eed4a2b545cb88cfc0234267d26c8
ninja drills <ghayek@ptgroup.com>
2023-03-13 08:11:28 +0200

    add time module

--- app.py
+++ app.py
@@ -27,59 +27,59 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
-@app.route('/Searchforuser', methods=['POST'])
-def search():
+# @app.route('/Searchforuser', methods=['POST'])
+# def search():
 
-        searchName = request.form.get('searchName')
-        count = 1
-        countfounded = 1
-        founded = False
+#         searchName = request.form.get('searchName')
+#         count = 1
+#         countfounded = 1
+#         founded = False
 
  
-        listFordisplayName = []
-        ListParsing = ["mail", "displayName", "userPrincipalName"]
+#         listFordisplayName = []
+#         ListParsing = ["mail", "displayName", "userPrincipalName"]
         
-        dicofDics = {}
-        dicofounded = {}
-        url = "https://prod-31.westeurope.logic.azure.com:443/workflows/0b121fa521f04f0aa6185a49dce36fa5/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=TJ5tk6JKlHQL4rqpaohEWe_zs1MLyL-nlQX7kCLwdQY"
+#         dicofDics = {}
+#         dicofounded = {}
+#         url = "https://prod-31.westeurope.logic.azure.com:443/workflows/0b121fa521f04f0aa6185a49dce36fa5/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=TJ5tk6JKlHQL4rqpaohEWe_zs1MLyL-nlQX7kCLwdQY"
 
-        payload={}
-        headers = {}
+#         payload={}
+#         headers = {}
 
-        response = requests.request("POST", url, headers=headers, data=payload)
-        jsonResponse = response.json()
-        jsonResponse = jsonResponse["value"]
+#         response = requests.request("POST", url, headers=headers, data=payload)
+#         jsonResponse = response.json()
+#         jsonResponse = jsonResponse["value"]
 
 
-        for items in jsonResponse:
-           currentElement =  []
-           currentElement.append(items['mail'])
-           currentElement.append(items['displayName'])
-           currentElement.append(items['userPrincipalName'])
-           dicofDics[count] = currentElement
-           count = count +1
-           listFordisplayName.append(currentElement)
+#         for items in jsonResponse:
+#            currentElement =  []
+#            currentElement.append(items['mail'])
+#            currentElement.append(items['displayName'])
+#            currentElement.append(items['userPrincipalName'])
+#            dicofDics[count] = currentElement
+#            count = count +1
+#            listFordisplayName.append(currentElement)
        
            
      
-        if(searchName == ''):
-            return render_template('Searchforuser.html', dicofDics = dicofDics)
+#         if(searchName == ''):
+#             return render_template('Searchforuser.html', dicofDics = dicofDics)
           
       
-        for item in dicofDics:
-            if(searchName in dicofDics[item][2] or searchName in dicofDics[item][1]):
-                dicofounded[countfounded] = dicofDics[item]
-                countfounded = countfounded + 1 
-                founded = True
+#         for item in dicofDics:
+#             if(searchName in dicofDics[item][2] or searchName in dicofDics[item][1]):
+#                 dicofounded[countfounded] = dicofDics[item]
+#                 countfounded = countfounded + 1 
+#                 founded = True
 
           
-        if founded:
-            dicofDics = dicofounded
+#         if founded:
+#             dicofDics = dicofounded
             
-            return render_template('Searchforuser.html', dicofDics = dicofDics)
-        else:
+#             return render_template('Searchforuser.html', dicofDics = dicofDics)
+#         else:
        
-            return "user not found"
+#             return "user not found"
 
 if __name__ == '__main__':
    app.run()
