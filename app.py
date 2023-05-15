c901b8d80cb6f08f48a6095af793c5bb19e73459
ninja drills <ghayek@ptgroup.com>
2023-05-15 08:00:28 +0300

    usersTemplate

--- app.py
+++ app.py
@@ -120,7 +120,38 @@ def search():
 
 
 
+@app.route('/runworkflow', methods=['POST'])
+def hellouser():
 
+
+
+   name = request.form.get('name')
+   time = request.form.get('time')
+   selectedElement = request.form.get('selectedElement')
+   url = "https://prod-208.westeurope.logic.azure.com:443/workflows/9cb9b9b69dad46f692b7804c3d546bc1/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jGeQ-GVf2nZ2p99KNJpGmVt-61T3IzimUduOdEizTCM"
+
+   payload = json.dumps({
+   "name": name,
+   "time": time,
+   "selectedElement": selectedElement
+   })
+   headers = {
+   'Content-Type': 'application/json'
+   }
+
+   response = requests.request("POST", url, headers=headers, data=payload)
+
+   print(response.text)
+
+
+
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('runworkflowuser.html', name = name, time = time,selectedElement=selectedElement)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 
 
